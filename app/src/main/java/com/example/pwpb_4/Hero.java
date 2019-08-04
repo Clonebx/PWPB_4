7a198acb8cc04a2ba7641082724df1e6e8842a63
Rif <clonebx.1@gmail.com>
2019-08-04 23:51:43 +0700

    commit

---
+++ app/src/main/java/com/example/pwpb_4/Hero.java
@@ -0,0 +1,20 @@
+package com.example.pwpb_4;
+
+public class Hero {
+    int image;
+    String nama;
+
+    public Hero(int image, String nama) {
+        this.image = image;
+        this.nama = nama;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
+
